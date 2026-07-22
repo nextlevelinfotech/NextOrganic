170b5be3f6bae590f3f35d047cddbed8d295f552
umesh-12 <umesh.kayastha8@gmail.com>
2026-07-22 10:43:45 +0545

    before delete fils

--- src/app/shared-services/Authguard/auth.guard.ts
+++ src/app/shared-services/Authguard/auth.guard.ts
@@ -2,14 +2,28 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../authService/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+// 1. ADMIN GUARD (Admin section protected garna)
+export const adminAuthGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  if (auth.isLoggedIn()) {
-    return true; // ✅ allow
+  if (auth.isAdminLoggedIn()) {
+    return true; // ✅ Allow Admin
   } else {
-    router.navigate(['/admin-login']); // ❌ redirect
+    router.navigate(['/admin-login']); // ❌ Redirect to Admin Login
+    return false;
+  }
+};
+
+// 2. CUSTOMER GUARD (User/Customer section protected garna)
+export const customerAuthGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (auth.isLoggedIn()) { // Customer Logged-in check
+    return true; // ✅ Allow Customer
+  } else {
+    router.navigate(['/login']); // ❌ Redirect to Customer Login
     return false;
   }
 };
